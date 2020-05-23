ae29bb642dc8ed7c5df8091dcfc6ba7bd039d183
sushil46in <58732294+sushil46in@users.noreply.github.com>
2020-05-23 12:31:16 +0200

    aks

--- Learning/main.tf
+++ Learning/main.tf
@@ -99,9 +99,9 @@ resource "azurerm_kubernetes_cluster" "aks" {
 }
 
 output "client_certificate" {
-  value = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+  value = azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.example.kube_config_raw
+  value = azurerm_kubernetes_cluster.aks.kube_config_raw
 }
