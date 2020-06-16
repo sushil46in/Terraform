b8f567d03f4fc55d53061974f6b54c9bf6a04765
sushil46in <58732294+sushil46in@users.noreply.github.com>
2020-06-16 19:59:55 +0200

    vm

--- Learning/main.tf
+++ Learning/main.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
   version         = "2.10.0"
   client_id       = "850e3cbc-1d00-4601-bbaa-9b88da7a3c07"
-  client_secret   = "266eda55-e1b5-49ba-b32e-9abcdb91d4a1"
+  client_secret   = "V5-.5j8Fn.-_2D6YYSOt7hkXE.H.-oO_k1"
   subscription_id = "c1612ae2-5f15-487e-b926-fecaf634a54c"
   tenant_id       = "348fd517-5819-4a07-aec8-7e3090490c8b"
   features {}
